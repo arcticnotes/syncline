5374134ea0ecc6108c0aad4bb093cf8c0386eb74
Paul <paul@arcticnotes.com>
2025-12-24 19:05:15 -0500

    add a trace-level option

--- lib/syncline.d.ts
+++ lib/syncline.d.ts
@@ -1,51 +1,59 @@
 import EventEmitter from 'node:events';
 
-export type SynclineSpawnOptions = {
+declare module '@arcticnotes/syncline' {
 
-	/**
-	 * Current working directory of the child process. Defaults to the CWD of the current process.
-	 */
-	cwd?: string,
+	export interface SynclineSpawnOptions {
 
-	/**
-	 * Environment variables. Defaults to the environment variables of the current process.
-	 */
-	env?: { [ key: string]: string},
+		/**
+		 * Current working directory of the child process. Defaults to the CWD of the current process.
+		 */
+		cwd?: string;
 
-	/**
-	 * `argv[0]`, defaults to the command.
-	 */
-	argv0?: string
-}
+		/**
+		 * Environment variables. Defaults to the environment variables of the current process.
+		 */
+		env?: { [ key: string]: string};
 
-export class Syncline extends EventEmitter {
+		/**
+		 * `argv[0]`, defaults to the command.
+		 */
+		argv0?: string;
 
-	/**
-	 * Spawns a child process with the specified command name and arguments, and returns a {@link Promise} that resolves
-	 * to a {@link Syncline}.
-	 *
-	 * @param command Command name
-	 * @param args Arguments, defaults to empty array
-	 * @param options Spawn options
-	 */
-	static async spawn( command: string, args?: string[], options?: SynclineSpawnOptions): Promise< Syncline> {
+		/**
+		 * Trace level, 0 = off, 3 = all.
+		 */
+		trace?: number;
 	}
 
-	/**
-	 * Sends a line of text to the child process and returns the next line from the child process.
-	 *
-	 * @param inputLine A line of input string
-	 * @param timeout Optional timeout in milliseconds
-	 */
-	exchange( inputLine: string, timeout?: number): string {
-	}
+	export class Syncline extends EventEmitter {
 
-	/**
-	 * Terminates the child process, if not already, and returns a {@link Promise} that resolves to one of the following:
-	 *
-	 * *  An object with property `code` set to the exit status code of the child process.
-	 * *  An object with property `signal` set to the signal that terminated the child process.
-	 */
-	async close(): Promise< { code: number} | { signal: string}> {
+		/**
+		 * Spawns a child process with the specified command name and arguments, and returns a {@link Promise} that resolves
+		 * to a {@link Syncline}.
+		 *
+		 * @param command Command name
+		 * @param args Arguments, defaults to empty array
+		 * @param options Spawn options
+		 */
+		static async spawn( command: string, args?: string[], options?: SynclineSpawnOptions): Promise< Syncline> {
+		}
+
+		/**
+		 * Sends a line of text to the child process and returns the next line from the child process.
+		 *
+		 * @param inputLine A line of input string
+		 * @param timeout Optional timeout in milliseconds
+		 */
+		exchange( inputLine: string, timeout?: number): string {
+		}
+
+		/**
+		 * Terminates the child process, if not already, and returns a {@link Promise} that resolves to one of the following:
+		 *
+		 * *  An object with property `code` set to the exit status code of the child process.
+		 * *  An object with property `signal` set to the signal that terminated the child process.
+		 */
+		async close(): Promise< { code: number} | { signal: string}> {
+		}
 	}
 }
--- test/test.js
+++ test/test.js
@@ -3,7 +3,8 @@ import TEST from 'node:test';
 import { Syncline} from '@arcticnotes/syncline';
 
 TEST( 'smoke test', async() => {
-	const syncline = await Syncline.spawn( 'stdbuf', [ '-oL', 'tr', 'a-z', 'A-Z']);
+	const syncline = await Syncline.spawn( 'stdbuf', [ '-oL', 'tr', 'a-z', 'A-Z'], { trace: 3});
 	ASSERT.equal( syncline.exchange( 'Hello, world!'), 'HELLO, WORLD!'); 
+	await new Promise( resolve => setTimeout( resolve, 500));
 	await syncline.close();
 });
