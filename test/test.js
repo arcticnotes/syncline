cb49242ccd5f0f966e6a25512f17450959cac5d1
Paul <paul@arcticnotes.com>
2025-12-29 13:10:11 -0500

    add input/output events

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ import { Syncline} from '@arcticnotes/syncline';
 
 TEST( 'smoke test', async() => {
 	const syncline = await Syncline.spawn( 'stdbuf', [ '-oL', 'tr', 'a-z', 'A-Z'], { trace: 3});
+	syncline.on( 'input', line => console.log( `input (length=${ line.length}):`, line));
+	syncline.on( 'output', line => console.log( `output (length=${ line.length}):`, line));
 	ASSERT.equal( syncline.exchange( 'Hello, world!'), 'HELLO, WORLD!'); 
 	await new Promise( resolve => setTimeout( resolve, 500));
 	await syncline.close();
